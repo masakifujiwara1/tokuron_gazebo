ec1b9ef6723f6667aa34a945a8440b20bf9ef40c
MasakiFujiwara <fmasaki2020@gmail.com>
2023-09-21 21:43:38 +0900

    update

--- launch/multi_turtlebot3_world_4.launch.py
+++ launch/multi_turtlebot3_world_4.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     world = os.path.join(
         get_package_share_directory('tokuron_gazebo'),
         'worlds',
-        'turtlebot3_world.world'
+        'empty_world.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
